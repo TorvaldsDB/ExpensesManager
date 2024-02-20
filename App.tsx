46249fd3d2bdc467889cebc6f431a26842e0ed9d
DuBing <bdu.wbrq@hongshan.com>
2024-02-20 18:08:34 +0800

    Finish 127 Adding Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ManageExpense from "./screens/ManageExpense";
+import RencentExpenses from "./screens/RencentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="RecentExpenses" component={RencentExpenses} />
+      <BottomTab.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/RencentExpenses.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RencentExpenses = () => {
+  return (
+    <View>
+      <Text>RencentExpenses</Text>
+    </View>
+  );
+};
+
+export default RencentExpenses;
+
+const styles = StyleSheet.create({});
